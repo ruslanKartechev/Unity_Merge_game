eba2deb95a22ba4c62578cf57b028c68b95e9f03
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-09-05 09:25:37 +0600

    DEV color gradient

--- Assets/Scripts/Game/Hunting/AimVisualizer.cs
+++ Assets/Scripts/Game/Hunting/AimVisualizer.cs
@@ -6,11 +6,16 @@ namespace Game.Hunting
 {
     public class AimVisualizer : MonoBehaviour
     {
+        [SerializeField] private float _alphaOffset = 0.2f;
+        [SerializeField] private float _distanceMin;
+        [SerializeField] private float _distanceMax;
+        [SerializeField] private Color _colorMin;
+        [SerializeField] private Color _colorMax;
+        
         [SerializeField] private ParticleSystem _fromParticles;
         [SerializeField] private ParticleSystem _toParticles;
         [SerializeField] private LineRenderer _lineRenderer;
         [SerializeField] private int _pointsCount;
-        private List<Vector3> _positions;
         private AimPath _path;
         
         public void Show(AimPath path)
@@ -39,6 +44,23 @@ namespace Game.Hunting
             }
             _fromParticles.transform.position = _path.start;
             _toParticles.transform.position = _path.end;
+            var distance = (_path.end - _path.start).magnitude;
+            var lerpVal = Mathf.InverseLerp(_distanceMin, _distanceMax, distance);
+            var color = Color.Lerp(_colorMin, _colorMax, lerpVal);
+            var gradient = new Gradient();
+            var colorKeys = new GradientColorKey[1]
+            {
+                new(color, 0)
+            };
+            var alphaKeys = new GradientAlphaKey[4]
+            {
+                new(0, 0),
+                new(1, _alphaOffset),
+                new(1, 1 - _alphaOffset),
+                new(0, 1)
+            };
+            gradient.SetKeys(colorKeys, alphaKeys);
+            _lineRenderer.colorGradient = gradient;
         }
     }
 }
--- Assets/Scripts/Game/UI/Merging/MergingPage.cs
+++ Assets/Scripts/Game/UI/Merging/MergingPage.cs
@@ -64,7 +64,7 @@ namespace Game.UI.Merging
         private void OnBuyButton()
         {
             _mergeManager.OnPurchase();
-            _buyPunchAnimator.PunchAnimate();
+            // _buyPunchAnimator.PunchAnimate();
         }
     }
 }
