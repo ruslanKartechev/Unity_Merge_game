0516201c830c9aacc6132fd370f692ebfa69c35f
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-10-02 16:22:54 +0600

    DEV added level 9, fixed animations

--- Assets/Scripts/Game/Hunting/PreyListener_Bike.cs
+++ Assets/Scripts/Game/Hunting/PreyListener_Bike.cs
@@ -19,6 +19,8 @@ namespace Game.Hunting
         
         public override void OnInit()
         { 
+            _preyAnimator.SetRandomAnimOffset();
+            _preyAnimator.PlayByName("Idle_1");
             _carParticles.Spawn();
         }
 
