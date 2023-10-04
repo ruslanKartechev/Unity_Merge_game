4977c6bb5275f2e45d82a717f0a3e8f437789f6e
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-10-04 19:16:08 +0600

    DEV added bush for environemt system. Added bush for deser

--- Assets/Scripts/Game/Hunting/HunterBushSpawner.cs
+++ Assets/Scripts/Game/Hunting/HunterBushSpawner.cs
@@ -5,16 +5,26 @@ namespace Game.Hunting
 {
     public class HunterBushSpawner : MonoBehaviour
     {
-        [SerializeField] private List<HuntersBush> _prefabs;
+        [SerializeField] private List<Data> _prefabData;
         private HuntersBush _spawned;
-        public HuntersBush Bush => _spawned;
-
+        
+        
         public HuntersBush SpawnBush(Vector3 position, Quaternion rotation)
         {
-            var prefab = _prefabs[0];
-            var instance = Instantiate(prefab,  position, rotation, null);
+            var env = GC.LevelRepository.GetLevel(GC.PlayerData.LevelIndex).Environment;
+            var prefab = _prefabData.Find(t => t.Environment == env);
+           
+            var instance = Instantiate(prefab.Prefab,  position, rotation, null);
             _spawned = instance;
             return _spawned;
+        }
+        
+        
+        [System.Serializable]
+        public class Data
+        {
+            public LevelEnvironment Environment;
+            public HuntersBush Prefab;
         }
     }
 }
