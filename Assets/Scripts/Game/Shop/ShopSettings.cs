cfde2906231459c06eceac6550c2683878b0140f
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-10-12 07:34:11 +0600

    DEV removed shop settings SO. Fixed Critical level bugs on levels 30 - 40

--- Assets/Scripts/Game/Shop/ShopSettings.cs
+++ Assets/Scripts/Game/Shop/ShopSettings.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 namespace Game.Shop
 {
-    [CreateAssetMenu(menuName = "SO/Shop/" + nameof(ShopSettings), fileName = nameof(ShopSettings), order = 0)]
-    public class ShopSettings : ScriptableObject, IShopSettings
+    // [CreateAssetMenu(menuName = "SO/Shop/" + nameof(ShopSettings), fileName = nameof(ShopSettings), order = 0)]
+    [System.Serializable]
+    public class ShopSettings : IShopSettings
     {
         [SerializeField] private int _maxLevel = -1;
         [SerializeField] private MergeItemSO _item;
