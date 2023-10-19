c0ae53b7c885ca1c38f642e53f83341bdf196936
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-10-19 11:38:11 +0600

    DEV improved bike colliders

--- Assets/Scripts/Game/Hunting/LandHunterMouth.cs
+++ Assets/Scripts/Game/Hunting/LandHunterMouth.cs
@@ -15,9 +15,6 @@ namespace Game.Hunting
         [Space(10)] 
         [SerializeField] private HunterCamTargetMover _lookTargetMover;
 
-        private const float UpOffset = 0.25f;
-        private const float BodyPosDistance = 0.6f;
-
         
         public override void BiteTo(Transform movable, Transform parent, Transform refPoint, Vector3 position)
         {
@@ -30,28 +27,7 @@ namespace Game.Hunting
             _headJoint.connectedBody = _headRb;
             _ragdollPositioner?.SetPosition();
         }
-        
-        // public override void BiteTo(Transform movable, Transform parent, Transform refPoint, Vector3 position)
-        // {
-        //     var joint = transform;
-        //     _lookTargetMover?.Follow();
-        //     position = parent.InverseTransformPoint(position);
-        //     var planePoint = new Vector2(position.x, position.z).normalized * _distance;
-        //
-        //     movable.position = parent.TransformPoint(parent.InverseTransformPoint(movable.position).normalized * BodyPosDistance);
-        //     movable.rotation = refPoint.rotation;
-        //     
-        //     joint.parent = parent;
-        //     var rotVec = parent.position - (joint.position - joint.forward * 10f);
-        //     rotVec.y = 0;
-        //     joint.rotation = Quaternion.LookRotation(rotVec);
-        //     
-        //     joint.localPosition = new Vector3(planePoint.x, joint.localPosition.y + UpOffset, planePoint.y);
-        //
-        //     _headJoint.connectedBody = _headRb;
-        //     _ragdollPositioner?.SetPosition();
-        //     
-        // }
+
     }
     
 }
