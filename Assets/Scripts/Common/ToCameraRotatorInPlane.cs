c9092c2405ad1d30b16a14b2fb814729ae2a2ca6
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-10-27 21:23:04 +0600

    DEV added level scenarios

---
+++ Assets/Scripts/Common/ToCameraRotatorInPlane.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class ToCameraRotatorInPlane : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        private Transform _cameraTr;
+        
+        private void OnEnable()
+        {
+            _cameraTr = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            var lookVec = (_target.position - _cameraTr.position);
+            // lookVec.y = 0;
+            _target.rotation = Quaternion.LookRotation(lookVec);
+            var angles = _target.eulerAngles;
+            angles.z = angles.x = 0;
+            _target.eulerAngles = angles;
+        }
+        
+    }
+}
