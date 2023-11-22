afb5e0e103dfc849e791a5851f6d6693294c42ed
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-11-22 09:38:20 +0600

    DEV fixed bike animation, fixed bike air target point, changed aim line arch for air units

--- Assets/Scripts/Game/Hunting/Prey/PreyBehaviour_BikeRun.cs
+++ Assets/Scripts/Game/Hunting/Prey/PreyBehaviour_BikeRun.cs
@@ -5,6 +5,7 @@ namespace Game.Hunting.Prey
 {
     public class PreyBehaviour_BikeRun : MonoBehaviour, IPreyBehaviour
     {
+        [SerializeField] private PreyAnimator _preyAnimator;
         [SerializeField] private CarWheelsController _carWheelsController;
         [SerializeField] private PackUnitLocalMover _localMover;
         [SerializeField] private PreyHealth _health;
@@ -15,7 +16,7 @@ namespace Game.Hunting.Prey
         {
             _carWheelsController.StopAll();
             _carWheelsController.StartMoving();
-            // _preyAnimator.Moving();
+            _preyAnimator.PlayByName("Idle");
             _health.Show();
             _carParticles.Play();
             _localMover.MoveToLocalPoint();
