e21f01a5a4cdbbc49cdf9de5220b6b6df10fdd4e
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-11-24 18:47:53 +0600

    DEV added analytics preprocessor condition

--- Assets/Scripts/Game/Levels/AnalyticsEvents.cs
+++ Assets/Scripts/Game/Levels/AnalyticsEvents.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#define SEND_EVENTS__
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Levels
@@ -7,11 +8,11 @@ namespace Game.Levels
     {
         public static string boss => "boss";
         public static string normal => "normal";
-        
-        
+
         public static void OnStarted(string levelType)
         {
             Debug.Log($"[Analytics] Start: {GC.PlayerData.LevelTotal + 1}");
+            #if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("level_start", new Dictionary<string, object>()
             {
                 { "level_number", GC.PlayerData.LevelTotal + 1 },
@@ -19,11 +20,13 @@ namespace Game.Levels
                 { "level_diff ", "easy" },
                 { "level_type", levelType },
             });
+            #endif
         }
         
         public static void OnFinished(string levelType)
         {
             Debug.Log($"[Analytics] Finish: {GC.PlayerData.LevelTotal + 1}");
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("level_finish", new Dictionary<string, object>()
             {
                 {"level_number", GC.PlayerData.LevelTotal+1},
@@ -31,13 +34,14 @@ namespace Game.Levels
                 {"level_name", "level_"},
                 {"level_diff ", "easy"},
                 {"level_type", levelType},
-                
             });    
+            #endif
         }
 
         public static void OnFailed(string levelType)
         {
             Debug.Log($"[Analytics] Fail: {GC.PlayerData.LevelTotal + 1}");
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("level_finish", new Dictionary<string, object>()
             {
                 {"level_number", GC.PlayerData.LevelTotal+1},
@@ -46,19 +50,23 @@ namespace Game.Levels
                 {"level_diff ", "easy"},
                 {"level_type", levelType},
             });      
+            #endif
         }
 
         public static void OnTutorial(string stepName)
         {
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("tutorial", new Dictionary<string, object>()
             {
                 {"step_name", stepName},
-            });   
+            });
+            #endif
         }
         
         public static void OnRestart(string levelType)
         {
             Debug.Log($"[Analytics] Restart: {GC.PlayerData.LevelTotal + 1}");
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("level_finish", new Dictionary<string, object>()
             {
                 {"level_number", GC.PlayerData.LevelTotal+1},
@@ -67,11 +75,13 @@ namespace Game.Levels
                 {"level_diff ", "easy"},
                 {"level_type", levelType},
             });    
+            #endif
         }
         
         public static void OnExited(string levelType)
         {
             Debug.Log($"[Analytics] Exit: {GC.PlayerData.LevelTotal + 1}");
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("level_finish", new Dictionary<string, object>()
             {
                 {"level_number", GC.PlayerData.LevelTotal+1},
@@ -80,29 +90,31 @@ namespace Game.Levels
                 {"level_diff ", "easy"},
                 {"level_type", levelType},
             });    
+            #endif
         }
 
         public static void OnEggPurchase(int level, float cost, string item)
         {
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("egg_purchase", new Dictionary<string, object>()
             {
                 { "egg_level", level},
                 { "egg_cost", cost },
                 { "item ", item }
             });
+            #endif
         }
 
         public static void OnGridCellPurchase(int x, int y, float cost)
         {
+#if SEND_EVENTS
             MadPixelAnalytics.AnalyticsManager.CustomEvent("grid_cell_purchase", new Dictionary<string, object>()
             {
                 { "cost", cost},
                 { "x", x },
                 { "y ", y }
             });
+            #endif
         }
-
-
-        
     }
 }
