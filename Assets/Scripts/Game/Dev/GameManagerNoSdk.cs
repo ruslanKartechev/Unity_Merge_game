1abcb6406626a25286b937379b70c3861a2e661b
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-11-24 18:43:55 +0600

    DEV added boot_2 scene with no SDK and simplified init logic

---
+++ Assets/Scripts/Game/Dev/GameManagerNoSdk.cs
@@ -0,0 +1,102 @@
+﻿using Common.Saving;
+using Game.Saving;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+namespace Game.Dev
+{
+    public class GameManagerNoSdk : MonoBehaviour
+    {
+        [SerializeField] private string _startPageName;
+        [SerializeField] private BootSettings _bootSettings;
+        [Header("For test")]
+        [SerializeField] private TestLoader _testLoader;
+        
+        private void Awake()
+        {
+            if (!GameState.FirstLaunch)
+            {
+                ShowStartScreen();
+                return;
+            }
+            GameState.SingleLevelMode = false;
+            GameState.FirstLaunch = false;
+            DontDestroyOnLoad(gameObject);
+            InitFramerate();
+            InitContainer();
+            InitSaves();
+            // var waitTime = 5f;
+            // _testLoader.Show();
+            // _testLoader.WaitAndCallback(waitTime, () =>
+            // {
+            //     _testLoader.Hide();
+            //     _pregamePage.ShowWithTermsPanel(PlayGame);
+            // });
+            PlayGame();
+        }
+
+        private void Start()
+        {
+            Facebook.Unity.FB.Init();
+        }
+
+        private void InitFramerate()
+        {
+            CLog.LogWHeader("GM", "Init frameRate", "w");
+            if(_bootSettings.CapFPS)
+                Application.targetFrameRate = _bootSettings.FpsCap;
+            else 
+                Application.targetFrameRate = 500;
+        }
+
+        private void InitContainer()
+        {
+            var containerLocator = gameObject.GetComponent<IGlobalContainerLocator>();
+            containerLocator.InitContainer();
+        }
+
+        private void InitSaves()
+        {
+            CLog.LogWHeader("GM", "Init saves", "w");
+            if (_bootSettings.ClearAllSaves)
+                GC.DataSaver.Clear();
+            var dataInit = gameObject.GetComponent<SavedDataInitializer>();
+            dataInit.InitSavedData();    
+            
+            if (_bootSettings.doPeriodicSave)
+            {
+                var saver = gameObject.GetComponent<IPeriodicDataSaver>();
+                saver.SetInterval(_bootSettings.dataSavePeriod);
+                saver.Begin();
+            }
+        }
+
+        private void PlayGame()
+        {
+            CLog.LogWHeader("GM", "Play game", "w");
+            if (GC.PlayerData.TutorPlayed_Attack == false && GC.PlayerData.LevelTotal == 0)
+            {
+                Debug.Log("Tutorial not played. Start game from lvl_0");
+                GC.PlayerData.LevelIndex = 0;
+                GC.LevelManager.LoadCurrent();
+            }
+            else
+            {
+                ShowStartScreen();
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            GC.DataSaver.Save();
+        }
+
+        private void ShowStartScreen()
+        {
+            CLog.LogWHeader("GM", "Show start screen", "w");
+            SceneManager.LoadScene(_startPageName);
+        }
+      
+    }
+}
