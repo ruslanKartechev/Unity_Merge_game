1011fb0a995dd0a6784b9e480c2c635462dd345f
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-11-30 23:35:59 +0600

    DEV added new scene

---
+++ Assets/Scripts/Creatives/CreativeBoat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Game.Hunting.HuntCamera;
+using Game.Hunting.Prey;
+using Game.Hunting.Prey.Interfaces;
+using UnityEngine;
+
+namespace Creatives
+{
+    public class CreativeBoat : MonoBehaviour, IPrey
+    {
+        public event Action<IPrey> OnKilled;
+        [SerializeField] private CamFollowTarget _camFollowTarget;
+        [SerializeField] private CarPassenger _passenger;
+        [SerializeField] private PreySettings _settings;
+
+        public void Init()
+        {
+            IsAvailableTarget = true;
+            PreySettings = _settings;
+        }
+
+        public void OnPackRun()
+        {
+        }
+
+        public void OnPackAttacked()
+        {
+        }
+
+        public float GetReward()
+        {
+            return 100;
+        }
+
+        public PreySettings PreySettings { get; set; }
+        public ICamFollowTarget CamTarget => _camFollowTarget;
+        public bool IsAvailableTarget { get; private set; }
+    }
+}
